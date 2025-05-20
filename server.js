3f398692be21fd282075c66b850bd277e278c231
ike <okaforikennapaul@gmail.com>
2025-05-20 11:03:49 +0100

    updated version

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const mongoose = require('mongoose')
-const {app, server} = require('./app.js')
-const dotenv = require('dotenv')
-dotenv.config({})
+const mongoose = require("mongoose");
+const { app, server } = require("./app.js");
+const dotenv = require("dotenv");
+dotenv.config({});
 
 const PORT = process.env.PORT || 3000;
 
-const DB = process.env.DATA_BASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
+const DB = process.env.DATA_BASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
   })
-  .then(()=>{
-    console.log(`Database connected`)
+  .then(() => {
+    console.log(`Database connected`);
   })
-  .catch((err)=>{
-    console.log(err)
-  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-server.listen(PORT, ()=>{
-    console.log(`app is listening to port ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`app is listening to port ${PORT}`);
+});
