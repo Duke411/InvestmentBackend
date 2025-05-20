6773022c3f91dbb5d9e60f69f20074a430586637
ike <okaforikennapaul@gmail.com>
2025-05-20 14:09:45 +0100

    updated version

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// const express = require("express");
+// const http = require("http");
+// const app = express();
+// const server = http.createServer(app);
+// const user = require("./routes/userRoutes");
+// const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+// app.use(
+//   cors({
+//     origin: ["https://tradeemma.netlify.app", "http://localhost:5173/"], // Your React app's URL
+//     credentials: true, // Allow sending cookies with credentials
+//   })
+// );
+// app.options("*", cors());
+
+// app.use("/invest/api/v1", user);
+
+// module.exports = { app, server };
+
 const express = require("express");
 const http = require("http");
 const app = express();
@@ -10,10 +34,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Corrected CORS
 app.use(
   cors({
-    origin: ["https://tradeemma.netlify.app", "http://localhost:5173/"], // Your React app's URL
-    credentials: true, // Allow sending cookies with credentials
+    origin: ["https://tradeemma.netlify.app", "http://localhost:5173"],
+    credentials: true,
   })
 );
 app.options("*", cors());
